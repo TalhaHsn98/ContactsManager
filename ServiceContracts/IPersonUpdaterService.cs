90a96697a22179eb0792bf370e77614a0f9e04a5
TalhaHsn98 <syedtalha.hussain98@gmail.com>
2025-12-16 21:00:39 -0500

    Interface Segregation SOLID 2

--- ServiceContracts/IPersonUpdaterService.cs
+++ ServiceContracts/IPersonUpdaterService.cs
@@ -8,47 +8,15 @@ namespace ServiceContracts
     /// <summary>
     /// Represents business logic for manipulating Perosn entity
     /// </summary>
-    public interface IPersonsService
+    /// Represents business logic for manipulating Perosn entity
+    /// </summary>
+    public interface IPersonsUpdaterService
     {
         /// <summary>
-        /// Addds a new person into the list of persons
+        /// Updates the specified person details based on the given person ID
         /// </summary>
-        /// <param name="personAddRequest">Person to add</param>
-        /// <returns>Returns the same person details, along with newly generated PersonID</returns>
-        Task<PersonResponse> AddPerson(PersonAddRequest? personAddRequest);
-
-
-        /// <summary>
-        /// Returns all persons
-        /// </summary>
-        /// <returns>Returns a list of objects of PersonResponse type</returns>
-        Task<List<PersonResponse>> GetAllPersons();
-
-
-        /// <summary>
-        /// Returns the person object based on the given person id
-        /// </summary>
-        /// <param name="personID">Person id to search</param>
-        /// <returns>Returns matching person object</returns>
-        Task<PersonResponse?> GetPersonByPersonID(Guid? personID);
-
-
-        /// <summary>
-        /// Returns all person objects that matches with the given search field and search string
-        /// </summary>
-        /// <param name="searchBy">Search field to search</param>
-        /// <param name="searchString">Search string to search</param>
-        /// <returns>Returns all matching persons based on the given search field and search string</returns>
-        Task<List<PersonResponse>> GetFilteredPersons(string searchBy, string? searchString);
-
-        Task<List<PersonResponse>> GetSortedPersons(List<PersonResponse> allpersons, string sortBy, SortOrderOptions sortOrder);
-
+        /// <param name="personUpdateRequest">Person details to update, including person id</param>
+        /// <returns>Returns the person response object after updation</returns>
         Task<PersonResponse> UpdatePerson(PersonUpdateRequest? personUpdateRequest);
-
-        Task<bool> DeletePerson(Guid? personID);
-
-        Task<MemoryStream> GetPersonsCSV();
-
-        Task<MemoryStream> GetPersonsExcel();
     }
 }
